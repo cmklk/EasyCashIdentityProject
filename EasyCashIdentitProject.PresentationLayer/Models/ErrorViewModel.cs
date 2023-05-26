1b1d5ddd0ef1c408525e8b9e24cebd1dd0ca6d25
emka1 <emka1@EMKA>
2023-05-26 21:26:18 +0300

    Kstmanların yazılması

---
+++ EasyCashIdentitProject.PresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyCashIdentitProject.PresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
